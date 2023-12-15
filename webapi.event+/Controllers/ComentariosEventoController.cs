503b50f675a1f57d4f7630ffc1094049033b03b0
Evelyn Oliveira Ribeiro <53143873876@info.local>
2023-12-15 09:13:49 -0300

    novo

---
+++ webapi.event+/Controllers/ComentariosEventoController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.CognitiveServices.ContentModerator;
+using System.Text;
+using webapi.event_.Domains;
+using webapi.event_.Repositories;
+
+namespace webapi.event_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ComentariosEventoController : ControllerBase
+    {
+        ComentariosEventoRepository comentario = new ComentariosEventoRepository();
+        private readonly ContentModeratorClient _contentModeratorClient;
+
+        public ComentariosEventoController(ContentModeratorClient contentModeratorClient)
+        {
+            _contentModeratorClient = contentModeratorClient;
+        }
+
+        [HttpPost("ComentarioIa")]
+        public async Task<IActionResult> PostIA(ComentariosEvento novoComentario)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(novoComentario.Descricao))
+                {
+                    return BadRequest("A descrisao do comentario nao pode estar vazio ou nulo");
+                }
+                using var stream = new MemoryStream(Encoding.UTF8.GetBytes(novoComentario.Descricao));
+                var moderationResult = await _contentModeratorClient.TextModeration
+                    .ScreenTextAsync("text/plain", stream, "por", false, false, null, true);
+                if (moderationResult.Terms != null)
+                {
+                    novoComentario.Exibe = false;
+                    comentario.Cadastrar(novoComentario);
+
+                }
+                else
+                {
+                    novoComentario.Exibe =  true;
+                    comentario.Cadastrar(novoComentario);
+                }
+                return StatusCode(201, novoComentario);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(comentario.Listar());
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("BuscarPorIdUsuario")]
+        public IActionResult GetByIdUser(Guid idUsuario, Guid idEvento)
+        {
+            try
+            {
+                return Ok(comentario.BuscarPorIdUsuario(idUsuario, idEvento));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(ComentariosEvento novoComentario)
+        {
+            try
+            {
+                comentario.Cadastrar(novoComentario);
+
+                return StatusCode(201, novoComentario);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                comentario.Deletar(id);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("ListarSomenteExibe")]
+        public IActionResult GetShow()
+        {
+            try
+            {
+                return Ok(comentario.ListarSomenteExibe());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+
+    }
+}
---
+++ webapi.event+/Controllers/PresencasEventoController.cs
@@ -0,0 +1,108 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using webapi.event_.Domains;
+using webapi.event_.Interfaces;
+using webapi.event_.Repositories;
+
+namespace eventplus_webapi.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class PresencasEventoController : ControllerBase
+    {
+        private IPresencasEventoRepository _presencasEventoRepository;
+
+        
+        public PresencasEventoController()
+        {
+            _presencasEventoRepository = new PresencasEventoRepository();
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(PresencasEvento presencasEvento)
+        {
+            try
+            {
+                _presencasEventoRepository.Cadastrar(presencasEvento);
+
+                return StatusCode(201);
+            }
+            catch (Exception erro)
+            {
+
+                return BadRequest(erro.Message);
+            }
+        }
+
+   
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_presencasEventoRepository.Listar());
+            }
+            catch (Exception erro)
+            {
+
+                return BadRequest(erro.Message);
+            }
+        }
+        /// <summary>
+        /// minha lista
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>minha lista</returns>
+
+        [HttpGet("{id}")]
+        public IActionResult GetByUser(Guid id)
+        {
+            try
+            {
+                return Ok(_presencasEventoRepository.ListarMinhas(id));
+
+            }
+            catch (Exception erro)
+            {
+
+                return BadRequest(erro.Message);
+            }
+        }
+
+    
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, PresencasEvento presencaEvento)
+        {
+            try
+            {
+                _presencasEventoRepository.Atualizar(id, presencaEvento);
+
+                return Ok();
+            }
+            catch (Exception erro)
+            {
+
+                return BadRequest(erro.Message);
+            }
+        }
+
+  
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                _presencasEventoRepository.Deletar(id);
+
+                return Ok();
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
+    }
+}
